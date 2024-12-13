3d4d944b8b2a74e2d79e5999e9ec241f03100cf2
McBossWithUwULoveUwUForever <168752421+LoveUwUBotForeverMcBoss1999@users.noreply.github.com>
2024-12-13 21:24:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,56 @@
 from flask import Flask, render_template
-import pymysql
+import random
 
 app = Flask(__name__)
 
-# Database configuration
-db_configuration = {
-    'host': 'db.aurex.lk',
-    'user': 'u11_GsZ09Z9Etg',
-    'password': 'FWUwF6O9OxGOj3O+L@Pcmy+L',
-    'database': 's11_McBoss'
-}
+# Sample data for the servers
+servers = [
+    {
+        'name': 'SkyBlock',
+        'uuid': 'd6cd4493-378f-47f2-b6e0-6c8d13018ce5',
+        'owner': 'manu',
+        'node': 'node1',
+        'ip': 'panel2.mcboss.top:25570',
+        'status': 'Online'  # Can be 'Online', 'Stopped', or 'Restarting'
+    },
+    {
+        'name': 'LifeSteal',
+        'uuid': 'db967e1a-9477-451b-b92d-f022d5b2fd1b',
+        'owner': 'manu',
+        'node': 'node1',
+        'ip': 'panel2.mcboss.top:25566',
+        'status': 'Stopped'
+    },
+    {
+        'name': 'hub',
+        'uuid': '77664d0d-24db-4639-977a-6fcb07b08017',
+        'owner': 'manu',
+        'node': 'node1',
+        'ip': 'panel2.mcboss.top:25567',
+        'status': 'Online'
+    },
+    {
+        'name': 'Proxy',
+        'uuid': 'a5479a08-c4b7-4c96-9c30-8ad2073c8864',
+        'owner': 'manu',
+        'node': 'node1',
+        'ip': 'panel2.mcboss.top:25565',
+        'status': 'Online',
+        'members': random.randint(10, 100)  # Random member count for demonstration
+    },
+    {
+        'name': 'SkyWars',
+        'uuid': '09afc51b-325c-4bea-b68c-3e7603e8b9a4',
+        'owner': 'manu',
+        'node': 'node1',
+        'ip': 'panel2.mcboss.top:25569',
+        'status': 'Restarting'
+    }
+]
 
 @app.route('/')
 def index():
-    # Connect to the database
-    connection = pymysql.connect(**db_configuration)
-    cursor = connection.cursor(pymysql.cursors.DictCursor)
-
-    # Query the database
-    cursor.execute("SELECT * FROM authme")
-    records = cursor.fetchall()
-
-    # Close connection
-    cursor.close()
-    connection.close()
-
-    return render_template('index.html', records=records)
+    return render_template('index.html', servers=servers)
 
 if __name__ == '__main__':
     app.run(debug=True)
